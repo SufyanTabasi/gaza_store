c06e902b7082aaeb19c7218f1ffa885efe9a1cfd
SufyanTabasi <sufyan2020209@gmail.com>
2023-09-09 12:55:03 +0300

    Part Two Front and dashboard

---
+++ routes/admin.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\Admin\AdminController;
+use Illuminate\Support\Facades\Route;
+
+Route::prefix('admin')->name('admin.')->group( function() {
+    Route::get('/',[AdminController::class,'index'])->name('index');
+
+});
+
+
+
+
+
+
+//
